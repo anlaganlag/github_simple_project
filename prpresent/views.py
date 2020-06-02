acc2b2009ebf74bc8ea8fd6d55fbf78d439228cc
gal <tanjhu@163.com>
2020-06-02 18:02:32 +0800

    用requests和json來獲取api內容

--- prpresent/views.py
+++ prpresent/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'home.html',{})
+    import requests
+    import json
+    api_request = requests.get('https://api.github.com/users?since=1598676')
+    api = json.loads(api_request.content)
+    return render(request,'home.html',{'api':api})
