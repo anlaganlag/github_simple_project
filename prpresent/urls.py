b1abeb302a0a428cf30a050f53dce5f054beb773
gal <tanjhu@163.com>
2020-06-03 14:07:30 +0800

    搜索功能

--- prpresent/urls.py
+++ prpresent/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('user/',views.user,name='user'),
 ]
--- prpresent/views.py
+++ prpresent/views.py
@@ -3,6 +3,18 @@ from django.shortcuts import render
 def home(request):
     import requests
     import json
-    api_request = requests.get('https://api.github.com/users?since=1598676')
+    api_request = requests.get('https://api.github.com/users?since=13')
     api = json.loads(api_request.content)
     return render(request,'home.html',{'api':api})
+
+def user(request):
+    if request.method == 'POST':
+        import requests
+        import json
+        user = request.POST['user']
+        user_request = requests.get('https://api.github.com/users/'+user)
+        username = json.loads(user_request.content)
+        return render(request,'user.html',{'user':user,"username":username})
+    else:
+        notfound = '請在搜索框中用關鍵詞搜索'
+        return render(request,'user.html',{'notfound':notfound})
